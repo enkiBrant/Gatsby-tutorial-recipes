74c4b8382a1cc3bfda9850953fd08b18ccdcdf2f
EnkiBrant <enki.brant@gmail.com>
2021-10-17 12:21:08 +0200

    fix error build on netilfy

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `tfq444dt5tcv`,
-        accessToken: process.env.CONTENTFUL_API_KEY,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     {
